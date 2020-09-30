d5e844e5041f642c9c28d2584bbf225168c090a1
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2020-09-30 21:23:57 +0200

    update kubernetes tests

--- examples/kubernetes_example/example_runtime.sh
+++ examples/kubernetes_example/example_runtime.sh
@@ -2,16 +2,20 @@
 set -e
 
 # setup cluster (will be used in all tests & runs)
-./kind create cluster
-
 cd examples/kubernetes_example
 
-# test
+# run non-distributed example without cluster in environment
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test
-RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test --features "distributed"
-
-# run
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run
+
+../../kind create cluster
+
+# run same tests with cluster in environment
+RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test
+RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run
+
+# run distributed tests
+RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test --features "distributed"
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run --features "distributed"
 
 ../../kind delete cluster
