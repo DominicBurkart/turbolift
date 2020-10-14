7e15b491c00dccfdc0436a6396b8b9b0413e9333
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2020-10-14 19:17:27 +0200

    correctly point to kind in k8s example script

--- examples/kubernetes_example/example_runtime.sh
+++ examples/kubernetes_example/example_runtime.sh
@@ -8,7 +8,7 @@ RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run
 
 # setup cluster (will be used in all tests & runs)
-../../kind create cluster
+../../../kind create cluster
 
 # run same tests with cluster in environment
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test
@@ -18,4 +18,4 @@ RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test --features distributed
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run --features distributed
 
-../../kind delete cluster
+../../../kind delete cluster
