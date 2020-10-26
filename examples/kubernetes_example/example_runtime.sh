49dffbacc0a0e8fa36a24d23ebe4208eb0265722
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2020-10-26 20:15:59 +0100

    don't capture stdout in tests

--- examples/kubernetes_example/example_runtime.sh
+++ examples/kubernetes_example/example_runtime.sh
@@ -6,18 +6,18 @@ set -e
 cd turbolift/examples/kubernetes_example
 
 # run non-distributed example without cluster in environment
-RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test
+RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test -- --nocapture
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run
 
 # setup cluster (will be used in all tests & runs)
 ../../../kind create cluster
 
 # run same tests with cluster in environment
-RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test
+RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test -- --nocapture
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run
 
 # run distributed tests
-RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test --features distributed
+RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly test --features distributed -- --nocapture
 RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo +nightly run --features distributed
 
 ../../../kind delete cluster
