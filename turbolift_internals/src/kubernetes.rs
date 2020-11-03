1476a67b24c9f52f794f23e22547ac9a2113521e
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2020-11-03 22:36:15 +0100

    include rust compiler deps

--- turbolift_internals/src/kubernetes.rs
+++ turbolift_internals/src/kubernetes.rs
@@ -266,7 +266,7 @@ fn make_image(
     let tar_path = build_dir_canonical.join(tar_file_name);
     let docker_file = format!(
         "FROM ubuntu:latest
-RUN apt-get update && apt-get install -y curl && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y curl gcc libssl-dev pkg-config && rm -rf /var/lib/apt/lists/*
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain nightly-2020-09-28
 ENV PATH=/root/.cargo/bin:$PATH
 COPY {} {}
