4c7f4a86134d50369620ace6f873200c835e18b9
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2021-06-05 19:49:25 +0200

    use stable API

--- turbolift_internals/src/kubernetes.rs
+++ turbolift_internals/src/kubernetes.rs
@@ -163,7 +163,7 @@ impl DistributionPlatform for K8s {
 
         // make ingress pointing to service
         let ingress = serde_json::json!({
-            "apiVersion": "networking.k8s.io/v1beta1",
+            "apiVersion": "networking.k8s.io/v1",
             "kind": "Ingress",
             "metadata": {
                 "name": ingress_name
@@ -175,18 +175,13 @@ impl DistributionPlatform for K8s {
                             "paths": [
                                 {
                                     "path": format!("/{}", app_name),
-                                    // "pathType": "Prefix",
-                                    // "backend": {
-                                    //     "service" : {
-                                    //         "name": app_name,
-                                    //         "port": {
-                                    //             "number": SERVICE_PORT
-                                    //         }
-                                    //     }
-                                    // }
                                     "backend": {
-                                        "serviceName": service_name,
-                                        "servicePort": SERVICE_PORT
+                                        "service" : {
+                                            "name": service_name,
+                                            "port": {
+                                                "number": SERVICE_PORT
+                                            }
+                                        }
                                     }
                                 }
                             ]
