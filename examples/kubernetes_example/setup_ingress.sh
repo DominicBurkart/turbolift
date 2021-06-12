b2ee4b0eeda397c389c622fcb852d9f625bf96cd
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2021-06-12 19:55:12 +0200

    add debug statements to debug ci ingress issue

--- examples/kubernetes_example/setup_ingress.sh
+++ examples/kubernetes_example/setup_ingress.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-set -e
-
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 sleep 90s # give k8s time to generate the pod ^
 printf "\n⏱️ waiting for ingress controller to be ready...\n"
@@ -8,3 +6,5 @@ kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=30m
+kubectl describe pods -A
+kubectl logs --all-containers=true -l app.kubernetes.io/component=controller
