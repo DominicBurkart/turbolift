f665ce4b746999fd30ff0366abd104a4b54798cb
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2021-06-12 23:11:03 +0200

    re-enable autoscale code

--- turbolift_internals/src/kubernetes.rs
+++ turbolift_internals/src/kubernetes.rs
@@ -240,25 +240,25 @@ impl DistributionPlatform for K8s {
 
         // todo make sure that the pod and service were correctly started before returning
 
-        // if self.max_scale_n > 1 {
-        //     // set autoscale
-        //     let scale_args = format!(
-        //         "autoscale deployment {} --max={}",
-        //         deployment_name, self.max_scale_n
-        //     );
-        //     let scale_status = Command::new("kubectl")
-        //         .args(scale_args.as_str().split(' '))
-        //         .status()?;
-        //
-        //     if !scale_status.success() {
-        //         return Err(anyhow::anyhow!(
-        //             "autoscale error: error code: {:?}",
-        //             scale_status.code()
-        //         )
-        //         .into());
-        //         // ^ todo attach error context from child
-        //     }
-        // }
+        if self.max_scale_n > 1 {
+            // set autoscale
+            let scale_args = format!(
+                "autoscale deployment {} --max={}",
+                deployment_name, self.max_scale_n
+            );
+            let scale_status = Command::new("kubectl")
+                .args(scale_args.as_str().split(' '))
+                .status()?;
+
+            if !scale_status.success() {
+                return Err(anyhow::anyhow!(
+                    "autoscale error: error code: {:?}",
+                    scale_status.code()
+                )
+                .into());
+                // ^ todo attach error context from child
+            }
+        }
 
         sleep(Duration::from_secs(90)).await;
         // todo implement the check on whether the service is running / pod failed
