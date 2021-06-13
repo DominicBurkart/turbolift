e530a1baae8cc12a6ed7be0e115fda2f42c8a07e
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2021-06-13 17:29:42 +0200

    try adding more build flags

--- turbolift_internals/build.rs
+++ turbolift_internals/build.rs
@@ -2,4 +2,6 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-cfg=procmacro2_semver_exempt");
     println!("cargo:rustc-cfg=span_locations");
+    println!("cargo:rustc-cfg=use_proc_macro");
+    println!("cargo:rustc-cfg=super_unstable");
 }
