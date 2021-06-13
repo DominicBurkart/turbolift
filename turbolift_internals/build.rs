118646ba0bf9385f08226282e87eee88f6c5e1d3
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2021-06-13 17:57:05 +0200

    decrease number of flags to what should be the essential

--- turbolift_internals/build.rs
+++ turbolift_internals/build.rs
@@ -1,11 +1,4 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-cfg=use_proc_macro");
-    println!("cargo:rustc-cfg=wrap_proc_macro");
-    println!("cargo:rustc-cfg=hygiene");
-    println!("cargo:rustc-cfg=proc_macro_span");
-    println!("cargo:rustc-cfg=super_unstable");
-    println!("cargo:rustc-cfg=span_locations");
     println!("cargo:rustc-cfg=procmacro2_semver_exempt");
-    println!("cargo:rustc-cfg=lexerror_display");
 }
