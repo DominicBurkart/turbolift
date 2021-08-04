e4e115291c9ec1e606ad6c82c93cd5a1685da803
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2021-08-04 21:41:20 +0200

    add example with multiple distributed functions

---
+++ examples/kubernetes_example_multiple_distributed/setup_cluster.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+# assumes that kind is already setup
+
+printf "\n😤 deleting current cluster if it exists\n"
+kind delete cluster
+
+printf "\n👷 setting up cluster with custom ingress-compatible config\n"
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
+kubectl cluster-info --context kind-kind
+echo "cluster initialized."
+
+
+printf "\n🚪 adding ingress\n"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/191b27a8bbc168422582375cf352f95ea89c2732/deploy/static/provider/kind/deploy.yaml
+sleep 90s # give k8s time to generate the pod ^
+printf "\n⏱️ waiting for ingress controller to be ready...\n"
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=30m
+echo "🚪 ingress ready."
